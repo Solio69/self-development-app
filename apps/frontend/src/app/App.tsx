e8ce8d45bf99af50e9104e4b8d035a089414d612
Anastasiya Kazanina <anastasiya.kazanina@ccsteam.ru>
2024-12-05 14:08:20 +0300

    [front]feature/added router

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { PATHS } from './routes/router'
+
+const router = createBrowserRouter([
+  {
+    path: PATHS.home,
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: PATHS.login,
+    element: <div>Login</div>,
+  },
+  {
+    path: PATHS.register,
+    element: <div>Register</div>,
+  },
+  {
+    path: PATHS.profile,
+    element: <div>Profile</div>,
+  },
+])
+
 function App() {
-  return <div className="App">Hello world!</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ apps/frontend/src/app/routes/router.ts
@@ -0,0 +1,6 @@
+export const PATHS = {
+  home: '/',
+  login: '/login',
+  register: '/register',
+  profile: '/profile',
+} as const
