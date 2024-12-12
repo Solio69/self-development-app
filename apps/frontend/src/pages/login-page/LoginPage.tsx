808eddaa06f37b3b2259318bde7352690b5f4b60
Anastasiya Kazanina <anastasiya.kazanina@ccsteam.ru>
2024-12-12 11:49:31 +0300

    [front] feature/added layout of login form

---
+++ apps/frontend/src/pages/login-page/LoginPage.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Form, Input } from 'antd'
+import { Link } from 'react-router-dom'
+import { PATHS } from '../../app/routes/router'
+import './LoginPage.scss'
+
+const LoginPage = () => {
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values)
+  }
+
+  return (
+    <section className="login-page">
+      <Form
+        name="login"
+        size="large"
+        initialValues={{ remember: true }}
+        style={{ width: 360 }}
+        onFinish={onFinish}
+        className="login-page__form"
+      >
+        <Form.Item
+          name="username"
+          className="login-page__form-item"
+          rules={[
+            { required: true, message: 'Логин обязетлен для заполнения!' },
+          ]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          className="login-page__form-item"
+          name="password"
+          rules={[
+            { required: true, message: 'Пароль обязетлен для заполнения!' },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item className="login-page__form-item">
+          <Button block type="primary" htmlType="submit">
+            Войти
+          </Button>
+        </Form.Item>
+        <Link className="login-page__form-item--link" to={PATHS.register}>
+          Регистрация
+        </Link>
+      </Form>
+    </section>
+  )
+}
+
+export default LoginPage
