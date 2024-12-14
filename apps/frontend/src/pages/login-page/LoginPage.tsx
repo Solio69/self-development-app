a4c8315d821d37e3e242af45a8ae25f4909432e4
Anastasiya Kazanina <anastasiya.kazanina@ccsteam.ru>
2024-12-14 15:04:16 +0300

    [front] fix/corrected login and registration pages

--- apps/frontend/src/pages/login-page/LoginPage.tsx
+++ apps/frontend/src/pages/login-page/LoginPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import { Button, Form, Input } from 'antd'
 import { Link } from 'react-router-dom'
@@ -11,45 +10,49 @@ const LoginPage = () => {
   }
 
   return (
-    <section className="login-page">
-      <Form
-        name="login"
-        size="large"
-        onFinish={onFinish}
-        className="login-page__form"
-      >
-        <Form.Item
-          name="email"
-          className="login-page__form-item"
-          rules={[
-            { required: true, message: 'Email обязетлен для заполнения!' },
-          ]}
+    <div className="login-page">
+      <section className="login-page__content">
+        <h1 className="register-page__title">Войти</h1>
+        <Form
+          name="login"
+          size="large"
+          onFinish={onFinish}
+          className="login-page__form"
         >
-          <Input prefix={<UserOutlined />} placeholder="Email" />
-        </Form.Item>
-        <Form.Item
-          className="login-page__form-item"
-          name="password"
-          rules={[
-            { required: true, message: 'Пароль обязетлен для заполнения!' },
-          ]}
-        >
-          <Input
-            prefix={<LockOutlined />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item className="login-page__form-item">
-          <Button block type="primary" htmlType="submit">
-            Войти
-          </Button>
-        </Form.Item>
-        <Link className="login-page__form-item--link" to={PATHS.register}>
-          Регистрация
-        </Link>
-      </Form>
-    </section>
+          <Form.Item
+            name="email"
+            className="login-page__form-item"
+            rules={[
+              { required: true, message: 'Email обязетлен для заполнения!' },
+            ]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            className="login-page__form-item"
+            name="password"
+            rules={[
+              { required: true, message: 'Пароль обязетлен для заполнения!' },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item className="login-page__form-item">
+            <Button block type="primary" htmlType="submit">
+              Войти
+            </Button>
+          </Form.Item>
+          <div className={'login-page__form-item-link'}>
+            <span>Еще не зарегистрированы?</span>
+            <Link to={PATHS.register}>Зарегистрироваться</Link>
+          </div>
+        </Form>
+      </section>
+    </div>
   )
 }
 
--- apps/frontend/src/pages/register-page/RegisterPage.tsx
+++ apps/frontend/src/pages/register-page/RegisterPage.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import { Button, Form, Input } from 'antd'
+import { PATHS } from '../../app/routes/router'
+import { Link } from 'react-router-dom'
 import './RegisterPage.scss'
 
 const RegisterPage = () => {
@@ -8,95 +9,98 @@ const RegisterPage = () => {
   }
 
   return (
-    <section className="register-page">
-      <Form
-        size="large"
-        layout="vertical"
-        className="register-page__form"
-        name="register"
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="email"
-          label="E-mail"
-          className="register-page__form-item"
-          rules={[
-            {
-              type: 'email',
-              message: 'Некорректный E-mail!',
-            },
-            {
-              required: true,
-              message: 'Укажите E-mail!',
-            },
-          ]}
+    <div className="register-page">
+      <section className="register-page__content">
+        <h1 className="register-page__title">Зарегистрироваться</h1>
+        <Form
+          size="large"
+          layout="vertical"
+          className="register-page__form"
+          name="register"
+          onFinish={onFinish}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          label="Пароль"
-          className="register-page__form-item"
-          rules={[
-            {
-              required: true,
-              message: 'Укажите пароль!',
-            },
-            {
-              min: 6,
-              message: 'Пароль должен содержать не менее 6 символов!',
-            },
-          ]}
-          hasFeedback
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          name="confirm"
-          label="Подтвердите пароль"
-          dependencies={['password']}
-          hasFeedback
-          className="register-page__form-item"
-          rules={[
-            {
-              required: true,
-              message: 'Подтвердите пароль!',
-            },
-
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve()
-                }
-                return Promise.reject(new Error('Пароли не совпадают!'))
+          <Form.Item
+            name="nickname"
+            tooltip="Имя которое будет использоваться в приложении"
+            className="register-page__form-item"
+            rules={[
+              {
+                required: true,
+                message: 'Укажите имя!',
+                whitespace: true,
               },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          name="nickname"
-          label="Имя"
-          tooltip="Имя которое будет использоваться в приложении"
-          className="register-page__form-item"
-          rules={[
-            {
-              required: true,
-              message: 'Укажите имя!',
-              whitespace: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item className="register-page__form-item">
-          <Button block type="primary" htmlType="submit">
-            Зарегистрироваться
-          </Button>
-        </Form.Item>
-      </Form>
-    </section>
+            ]}
+          >
+            <Input placeholder="Имя" />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            className="register-page__form-item"
+            rules={[
+              {
+                type: 'email',
+                message: 'Некорректный Email!',
+              },
+              {
+                required: true,
+                message: 'Укажите Email!',
+              },
+            ]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            className="register-page__form-item"
+            rules={[
+              {
+                required: true,
+                message: 'Укажите пароль!',
+              },
+              {
+                min: 6,
+                message: 'Пароль должен содержать не менее 6 символов!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="Пароль" />
+          </Form.Item>
+          <Form.Item
+            name="confirm"
+            dependencies={['password']}
+            hasFeedback
+            className="register-page__form-item"
+            rules={[
+              {
+                required: true,
+                message: 'Подтвердите пароль!',
+              },
+
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('Пароли не совпадают!'))
+                },
+              }),
+            ]}
+          >
+            <Input.Password placeholder="Подтвердите пароль" />
+          </Form.Item>
+          <Form.Item className="register-page__form-item">
+            <Button block type="primary" htmlType="submit">
+              Зарегистрироваться
+            </Button>
+          </Form.Item>
+          <div className={'register-page__form-item-link'}>
+            <span>У вас уже есть аккаунт?</span>
+            <Link to={PATHS.login}>Войдите</Link>
+          </div>
+        </Form>
+      </section>
+    </div>
   )
 }
 
