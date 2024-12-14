0c0d57fe44b792e93590d7a5c9d1246b23409a6e
Anastasiya Kazanina <anastasiya.kazanina@ccsteam.ru>
2024-12-14 13:44:41 +0300

    [front] fix/changed the location of the folder with the registration page

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom'
 import HomePage from '../pages/home-page/HomePage'
 import LoginPage from '../pages/login-page/LoginPage'
-import RegisterPage from '../pages/home-page/register-page/RegisterPage'
+import RegisterPage from '../pages/register-page/RegisterPage'
 import './styles/globals.scss'
 
 function App() {
