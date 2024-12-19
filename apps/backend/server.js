01b85f62445531ff887eb0f6085041159a96bdb5
Anastasiya Kazanina <anastasiya.kazanina@ccsteam.ru>
2024-12-19 12:12:54 +0300

    [back] fix/added cors

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const server = express()
 
 server.use(logger('dev'))
+server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 server.use(cookieParser())
